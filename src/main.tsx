efef569493793f3b530b848130334f1f87355af1
loic-marigny <loic.marigny@edu.devinci.fr>
2025-11-27 17:48:49 +0200

    Fix web path

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,11 @@ import App from './App'
 import { I18nProvider } from './i18n/I18nProvider'
 
 
+const basename = (import.meta.env.BASE_URL ?? '/') as string;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <I18nProvider>
         <App />
       </I18nProvider>
